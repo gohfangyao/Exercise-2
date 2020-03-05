f94e2bacb5fad3d9d53a6168bf86233a55debb87
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-03-05 15:40:55 +0800

    Update SquareCube.java

--- SquareCube.java
+++ SquareCube.java
@@ -1,17 +1,9 @@
-public class SquareCube {
-public static void main(String[]args)
-{
-	final int LOWER=0;
-	final int UPPER=10;
-	
-	System.out.println("Number\t\t\tSquare");
-	System.out.println("\tCube");
-	
-	for(int i=LOWER; i!=UPPER; i++)
-	{
-		
-		System.out.println(i+"\t\t\t"+ (int)Math.pow(1, 2));
-		System.out.println("\t"+(int)Math.pow(i, 3));
-	}
-}
+public class SquareCube{
+    public static void main(String[]args){
+        System.out.println("Number"+"\t\t"+"Square"+"\t\t"+"Cube");
+        for(int i=0;i<=10;i++){
+            System.out.println(i+"\t\t"+(int)Math.pow(i,2)+"\t\t"+(int)Math.pow(i,3));
+
+        }
+    }
 }
