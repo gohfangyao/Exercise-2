50d7f3c84b48aff60880f9a35ec3b6ad669bd266
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-03-29 17:33:08 +0800

    Update RandomInRange.java

--- RandomInRange.java
+++ RandomInRange.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 import java.util.Random;
-class RandomInRange
+public class RandomInRange
 {
-	public static void main(String[]args)
-	{
-		Scanner in= new Scanner(System.in);
-		System.out.print("Enter the lower limit:");
-		int lower= in.nextInt();
-		in.nextLine();
-		int upper = 0;
-		new RandomInRange(lower,upper);
-	}
+     public static void main(String[] args) 
+     {
+        Scanner sc = new Scanner(System.in); 
+        
+        System.out.print("Enter lower limit : ");
+        int lower = sc.nextInt();
+        sc.nextLine(); 
+        System.out.print("Enter upper limit : ");
+        int upper = sc.nextInt();
+        sc.nextLine();
 
-public RandomInRange(int lower, int upper)
-{
-	int rng=(lower>upper)?0 : lower + new Random(System.currentTimeMillis()).nextInt((upper-lower)+1);
-	System.out.println("Number generated is:" + rng);
-	}
-}
+        
+        new RandomInRange(lower, upper); 
+     } 
+     
+     public RandomInRange(int lower, int upper) 
+     {
+     
+           int range = (lower > upper) ? 0 : lower + new Random(System.currentTimeMillis()).nextInt((upper - lower) + 1);
+           
+           System.out.println("The Random Number is : " + range); 
+     } 
+    } 
